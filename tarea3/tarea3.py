f8888f7175d9de68f32cd7917a44987e0ee140ad
christian <christian@PC>
2015-01-29 22:49:31 -0430

    Marzullo's algorithm adapted to just care about the subinterval of the reservation requested.

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -5,37 +5,43 @@ Autores: Carlos Martínez - 11-10584
          Christian Teixeira - 11-11016
 '''
 
-def marzullo(sourcestable):
+def marzullo(sourcestable, intervbegin, intervend):
     tuples = []
     for source in sourcestable:
-        tuples.append([source[0], -1])
-        tuples.append([source[1], 1])
+        if (source[1] > intervbegin) and (source[0] < intervend):
+            tuples.append([source[0], -1])
+            tuples.append([source[1], 1])
     sortedtable = tuplesort(tuples)
+    
     best = 0
     cnt = 0
+    beststart = 0
+    bestend = 0
     for i in range(len(sortedtable)):
-        cnt = cnt - sortedtable[i][1]
-        if cnt > best:
-            best = cnt
-            beststart = sortedtable[i][0]
-            bestend = sortedtable[i+1][0]
-    occupies = 0
-    for source in sourcestable:
-        if source[0] > beststart or source[1]:
-            pass
-        pass
+            cnt = cnt - sortedtable[i][1]
+            if cnt > best:
+                best = cnt
+                beststart = sortedtable[i][0]
+                bestend = sortedtable[i+1][0]
+    
+    bestinterval = [beststart, bestend, best]
+    
+    return bestinterval
+    
     
 def tuplesort(tuples):
     return sorted(tuples, key = getKey)
 
+
 def getKey(item):
     return item[0]
 
+
 if __name__ == '__main__':
     reservations = []
+    reservations.append([8, 9])
     reservations.append([8, 12])
-    reservations.append([11,13])
-    reservations.append([14, 15])
-    bestinterval = marzullo(reservations)
-    print("Best interval is [" + str(bestinterval[0]) + "," + str(bestinterval[1]) + "]")
+    reservations.append([10, 12])
+    bestinterval = marzullo(reservations, 9, 10)
+    print("Best interval is [" + str(bestinterval[0]) + "," + str(bestinterval[1]) + "] and the number of reservations is: " + str(bestinterval[2]))
     
