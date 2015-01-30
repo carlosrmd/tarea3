c590227e403a3fdf191e98fb6b7bdf9973f32a5f
Carlos <carlosrmd94@gmail.com>
2015-01-29 23:30:24 -0430

    test_CarJustLeft and test_ParkingTotallyFull added

--- tarea3/testsuite.py
+++ tarea3/testsuite.py
@@ -1,9 +1,17 @@
 import unittest
-from functions import compute_payment
-from Tarifa import Tarifa
-from datetime import datetime
+from tarea3 import *
 
 class TestSuite(unittest.TestCase):
 	def test_CarJustLeft(self):
+		reservations = []
 		for i in range(9):
+			reservations.append([8,12])
+		reservations.append([8,10])
+		self.assertEqual(True, marzullo(reservations, 10, 12))
 			
+	def test_ParkingTotallyFull
+		reservations = []
+		for i in range(10):
+			reservations.append([6,18])
+		self.assertEqual(False, marzullo(reservations, 8, 12))
+
